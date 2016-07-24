3444d4586d6fa6787cc776f39203972849e71e6d
Abhishek Jain <abhi2000@gmail.com>
2016-07-24 22:44:35 +0100

    wired up reducer

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import styles from './home.less';
+import { connect } from 'react-redux';
+import { increase, decrease } from '../actionReducers/count';
 
 export default class Home extends React.Component {
+    onClick() {
+        this.props.increase(1);
+    }
     render() {
         return (
-            <div className={styles.home}>Home</div>
+            <div className={styles.home}>
+                { this.props.number }
+                <button onClick={ this.onClick.bind(this) }>Click</button>
+            </div>
         );
     }
 }
+Home.propTypes = {
+    number: React.PropTypes.number,
+    increase: React.PropTypes.func
+};
+export default connect(
+    state => ({ number: state.count.number }),
+    { increase, decrease }
+)(Home);
