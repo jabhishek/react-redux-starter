d78b92a92e884917089bc434412a6f54102b2705
Abhishek Jain <abhi2000@gmail.com>
2016-09-16 20:49:26 +0100

    Make the input controlled

--- src/components/trades-form/trades-form.js
+++ src/components/trades-form/trades-form.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react';
 import TextField from 'material-ui/TextField';
 
 export default class TradesForm extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			codeValue: ''
+		};
+	}
+
+	onCodeChange(event) {
+		this.setState({
+			codeValue: event.target.value,
+		});
+	}
+
 	addTrade() {
 
 	}
@@ -11,9 +25,10 @@ export default class TradesForm extends Component {
 			<div className="trades-form">
 				<form onSubmit={ this.addTrade }>
 					<TextField
-						hintText="Hint Text"
-						floatingLabelText="Floating Label Text"
-					/><br />
+						hintText="Enter code"
+						floatingLabelText="Security Code"
+						value={ this.state.codeValue }
+						onChange={ this.onCodeChange.bind(this) } />
 				</form>
 			</div>
 		);
