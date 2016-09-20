d11e0bbce52835f51680082bb5ccf33f94abd775
Abhishek Jain <abhi2000@gmail.com>
2016-09-20 22:22:55 +0100

    Converted portfolio page into stateless component

--- src/components/portfolio/portfolios.js
+++ src/components/portfolio/portfolios.js
@@ -1,14 +1,11 @@
 import React, { Component, PropTypes } from 'react';
 
-export default class PortfolioPage extends Component {
-	render() {
-		return (
-			<div className="container">
-				{ this.props.portfolios.length } portfolios
-			</div>
-		);
-	}
-}
-PortfolioPage.propTypes = {
-	portfolios: PropTypes.array
+const PortfolioPage = (portfolios) => {
+	return (
+		<div className="container">
+			{ portfolios.length } portfolios
+		</div>
+	);
 };
+export default PortfolioPage;
+
