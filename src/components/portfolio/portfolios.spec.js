0f75c5fecad2a850a81316c6db48eb33f960e551
Abhishek Jain <abhi2000@gmail.com>
2016-10-02 01:29:31 +0100

    Added unit tests

--- src/components/portfolio/portfolios.spec.js
+++ src/components/portfolio/portfolios.spec.js
@@ -3,12 +3,60 @@ import { shallow } from 'enzyme';
 import PortfolioPage from './portfolios';
 
 describe('Portfolios', () => {
-	let wrapper;
+	let wrapper, callParams;
+	let callCount;
+	function setup(portfolios) {
+		callCount = 0;
+		callParams = null;
+		const props = {
+			portfolios: portfolios,
+			portfolioActions: {
+				getPortfolios: (...args) => {
+					callCount++;
+					callParams = args;
+				}
+			}
+		};
+		wrapper = shallow(<PortfolioPage {...props}/>);
+
+		return {
+			wrapper,
+			props
+		}
+	}
+
 	beforeEach(() => {
-		wrapper = shallow(<PortfolioPage portfolios={[]} portfolioActions={{ getPortfolios: () => { }}}/>);
+		setup([]);
 	});
 
 	it('should exist', () => {
-		expect(wrapper.find('.container')).toBeDefined();
+		expect(wrapper.find('.container').length).toEqual(1);
 	})
+	it('should have AddPortfolio component', () => {
+		expect(wrapper.find('AddPortfolio').length).toEqual(1);
+	})
+	it('should have a separator', () => {
+		expect(wrapper.find('.separator').length).toEqual(1);
+	})
+	describe('SubHeader', () => {
+		it('should have SubHeader component', () => {
+			expect(wrapper.find('SubHeader').length).toEqual(1);
+		})
+		it('should have headerText property', () => {
+			const props = wrapper.find('SubHeader').props();
+			expect(props.headerText).toEqual('All Portfolios');
+		})
+	})
+	describe('PortfolioList', () => {
+		it('should be present', () => {
+			const { wrapper } = setup();
+			expect(wrapper.find('PortfolioList').length).toEqual(1);
+		})
+		it('should have portfolios property set as ', () => {
+			const { wrapper } = setup(['XYZ']);
+			const props = wrapper.find('PortfolioList').props();
+			expect(props.portfolios).toEqual(['XYZ']);
+		})
+	})
+
 });
